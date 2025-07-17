a18e57caa5d034af816d4078e478391dc669fe44
Exerdon <tushar84391@gmail.com>
2025-07-18 00:35:22 +0530

    new.py

---
+++ new.py
@@ -0,0 +1,6 @@
+a=input(" enter 'y' or 'n' /n 'y' to print 'n' to not print:- " )
+if(a== "y"):
+    print("hello world")
+else: # it will print the below line no matter any word would be entered other then 'y'
+    print("nothing would be printed u may go : )")
+print("thankuuuu ;* for comming") 
